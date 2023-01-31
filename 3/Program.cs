989b3c3245b6b121b3a4c1d90261accaeac2fc00
Your Name <you@example.com>
2023-02-01 00:47:09 +0300

    Добавила решение 3 задачи

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, -5, 0, -11 -> [1, 2, 5, 7, 19, -5, 0, -11]
+
+int[] GenerateArr(int n, int min, int max)
+{
+    int[] arrNum = new int[n];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < arrNum.Length; arrNum[i++] = rnd.Next(min, max + 1)) { }
+
+    return arrNum;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write($"[{String.Join(",", array)}]\n");
+}
+
+
+int[] array = GenerateArr(8, -100, 101);
+PrintArray(array);
